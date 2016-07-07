172cffda35c88237e23d43a22b52bcc2f6d9a124
xiaofanwu <xwu4@wellesley.edu>
2016-07-07 10:58:25 -0700

    fixed unit test to make sure that page UI does not sort but fire event to the server side.

--- test/px-data-table-test.js
+++ test/px-data-table-test.js
@@ -499,6 +499,390 @@ var data =
     "color": "rgb(218,74,95)"
   }
 ];
+var dataTest =
+[
+  {
+    "index": 0,
+    "name": "Liz Grimes",
+    "first": "Isabel long name to test left elipse",
+    "last": "Conrad",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/enda/73.jpg",
+    "boolean": true,
+    "guid": "8ea90ed8-83e1-4a28-a371-e5c0e1d0e021",
+    "integer": 28,
+    "date": "Sun Aug 14 1994 03:27:03 GMT-0700 (PDT)",
+    "address": "3 Vista Place",
+    "city": "Longbranch",
+    "state": "North Carolina",
+    "zip": 64470,
+    "country": "Lesotho",
+    "email": "isabelconrad@scentric.com",
+    "phone": "(817) 426-2901",
+    "color": "rgb(76,120,108)"
+  },
+  {
+    "index": 1,
+    "name": "Frazier Lara",
+    "first": "Jocelyn",
+    "last": "Wooten",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/guillogo/73.jpg",
+    "boolean": false,
+    "guid": "7017f9b1-690e-4c9d-8eee-f63285ca5148",
+    "integer": 10,
+    "date": "Tue May 24 1988 14:10:20 GMT-0700 (PDT)",
+    "address": "2 Catherine Street",
+    "city": "Healy",
+    "state": "Connecticut",
+    "zip": 41121,
+    "country": "Mongolia",
+    "email": "jocelynwooten@scentric.com",
+    "phone": "(901) 441-2582",
+    "color": "rgb(134,91,60)"
+  },
+  {
+    "index": 2,
+    "name": "Dora Griffith",
+    "first": "Valentine",
+    "last": "Meyer",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/iboldurev/73.jpg",
+    "boolean": true,
+    "guid": "8c3bd9ad-e8d6-4ea4-85e6-d145295f3f91",
+    "integer": 99,
+    "date": "Sat Feb 17 1973 03:45:57 GMT-0800 (PST)",
+    "address": "4 Whitty Lane",
+    "city": "Nicholson",
+    "state": "South Carolina",
+    "zip": 41343,
+    "country": "Germany",
+    "email": "valentinemeyer@scentric.com",
+    "phone": "(956) 428-2996",
+    "color": "rgb(201,142,97)"
+  },
+  {
+    "index": 3,
+    "name": "Shelley Molina",
+    "first": "Silva",
+    "last": "Alexander",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/smalonso/73.jpg",
+    "boolean": true,
+    "guid": "37224065-ac71-4716-be9a-108ecddfee47",
+    "integer": 16,
+    "date": "Wed Aug 02 1995 07:03:12 GMT-0700 (PDT)",
+    "address": "2 Berkeley Place",
+    "city": "Manila",
+    "state": "Michigan",
+    "zip": 22009,
+    "country": "Thailand",
+    "email": "silvaalexander@scentric.com",
+    "phone": "(823) 415-2224",
+    "color": "rgb(113,105,251)"
+  },
+  {
+    "index": 4,
+    "name": "Rita Lopez",
+    "first": "Hopkins",
+    "last": "Wong",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/taherrapee/73.jpg",
+    "boolean": false,
+    "guid": "1ea0922e-179d-4057-abeb-d8fe63e55da8",
+    "integer": 94,
+    "date": "Wed Sep 21 2011 04:03:55 GMT-0700 (PDT)",
+    "address": "4 Tompkins Avenue",
+    "city": "Movico",
+    "state": "Maine",
+    "zip": 89440,
+    "country": "Seychelles",
+    "email": "hopkinswong@scentric.com",
+    "phone": "(814) 488-2063",
+    "color": "rgb(133,126,66)"
+  },
+  {
+    "index": 5,
+    "name": "Wagner Shelton",
+    "first": "Harriet",
+    "last": "Sherman",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/ismailmayat/73.jpg",
+    "boolean": true,
+    "guid": "46a4a1bc-97af-46e0-aedd-c3cc42e6b8f5",
+    "integer": 27,
+    "date": "Wed Jul 07 2010 05:48:57 GMT-0700 (PDT)",
+    "address": "3 Stratford Road",
+    "city": "Makena",
+    "state": "Kansas",
+    "zip": 21389,
+    "country": "Chad",
+    "email": "harrietsherman@scentric.com",
+    "phone": "(887) 497-3612",
+    "color": "rgb(195,89,110)"
+  },
+  {
+    "index": 6,
+    "name": "Bethany Adkins",
+    "first": "Stacie",
+    "last": "Bartlett",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/carlyson/73.jpg",
+    "boolean": false,
+    "guid": "17aac57d-4644-44d8-8a6b-b2eedd3d42dc",
+    "integer": 49,
+    "date": "Sun Mar 14 1999 23:13:33 GMT-0800 (PST)",
+    "address": "4 Luquer Street",
+    "city": "Newcastle",
+    "state": "Iowa",
+    "zip": 72190,
+    "country": "Uruguay",
+    "email": "staciebartlett@scentric.com",
+    "phone": "(957) 412-3261",
+    "color": "rgb(164,84,221)"
+  },
+  {
+    "index": 7,
+    "name": "Angelia Rowe",
+    "first": "Charlene",
+    "last": "Stevens",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/thewillbeard/73.jpg",
+    "boolean": true,
+    "guid": "f3c6a195-d50e-479f-9c2a-c54c2a629852",
+    "integer": 13,
+    "date": "Sat Jan 04 1992 04:10:22 GMT-0800 (PST)",
+    "address": "4 Tiffany Place",
+    "city": "Dargan",
+    "state": "Vermont",
+    "zip": 31427,
+    "country": "Burkina Faso",
+    "email": "charlenestevens@scentric.com",
+    "phone": "(946) 404-2511",
+    "color": "rgb(99,147,74)"
+  },
+  {
+    "index": 8,
+    "name": "Knox Rivers",
+    "first": "Reyes",
+    "last": "Hartman",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/tgormtx/73.jpg",
+    "boolean": true,
+    "guid": "88e71d65-5a7a-4973-a1dc-9f5c1df485c8",
+    "integer": 41,
+    "date": "Sat May 28 1994 15:27:35 GMT-0700 (PDT)",
+    "address": "3 Wythe Place",
+    "city": "Coldiron",
+    "state": "Nebraska",
+    "zip": 76971,
+    "country": "Greenland",
+    "email": "reyeshartman@scentric.com",
+    "phone": "(910) 533-3832",
+    "color": "rgb(99,55,193)"
+  },
+  {
+    "index": 9,
+    "name": "Warren Scott",
+    "first": "Alissa",
+    "last": "Duncan",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/andresenfredrik/73.jpg",
+    "boolean": true,
+    "guid": "dfc23019-ab40-4597-91fe-74bcf60a9e2b",
+    "integer": 54,
+    "date": "Sun Feb 08 2009 21:57:53 GMT-0800 (PST)",
+    "address": "4 Henderson Walk",
+    "city": "Oley",
+    "state": "Florida",
+    "zip": 38878,
+    "country": "Fiji",
+    "email": "alissaduncan@scentric.com",
+    "phone": "(876) 584-3746",
+    "color": "rgb(218,58,52)"
+  },
+  {
+    "index": 10,
+    "name": "Mai Shepherd",
+    "first": "Christina",
+    "last": "Craft",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/mwarkentin/73.jpg",
+    "boolean": false,
+    "guid": "7b829e11-da52-4302-a645-4711c9af7df2",
+    "integer": 40,
+    "date": "Sat Apr 18 1992 20:34:36 GMT-0700 (PDT)",
+    "address": "3 Ocean Court",
+    "city": "Fairmount",
+    "state": "New Jersey",
+    "zip": 95002,
+    "country": "Viet Nam",
+    "email": "christinacraft@scentric.com",
+    "phone": "(842) 503-2389",
+    "color": "rgb(181,253,141)"
+  },
+  {
+    "index": 11,
+    "name": "Flora Tran",
+    "first": "Burt",
+    "last": "Landry",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/herkulano/73.jpg",
+    "boolean": false,
+    "guid": "3efeb9cc-5705-4e17-b800-42ee8e354d30",
+    "integer": 64,
+    "date": "Fri Apr 30 1982 14:23:53 GMT-0700 (PDT)",
+    "address": "2 Butler Street",
+    "city": "Walker",
+    "state": "Wisconsin",
+    "zip": 56241,
+    "country": "France, Metropolitan",
+    "email": "burtlandry@scentric.com",
+    "phone": "(914) 461-3510",
+    "color": "rgb(95,147,193)"
+  },
+  {
+    "index": 12,
+    "name": "Crawford Jordan",
+    "first": "Baldwin",
+    "last": "Hammond",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/jonny_moon/73.jpg",
+    "boolean": true,
+    "guid": "758e8af9-6a3c-47e1-a062-b49e6263b6e8",
+    "integer": 80,
+    "date": "Sat Sep 22 2012 18:24:55 GMT-0700 (PDT)",
+    "address": "3 Preston Court",
+    "city": "Vincent",
+    "state": "Washington",
+    "zip": 47560,
+    "country": "Uganda",
+    "email": "baldwinhammond@scentric.com",
+    "phone": "(800) 559-2165",
+    "color": "rgb(175,176,59)"
+  },
+  {
+    "index": 13,
+    "name": "Stanley Williams",
+    "first": "Laverne",
+    "last": "Hampton",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/alyssalowww/73.jpg",
+    "boolean": false,
+    "guid": "718262e9-01e7-4016-b068-6edd11a14a38",
+    "integer": 62,
+    "date": "Sat Sep 27 1975 16:25:15 GMT-0700 (PDT)",
+    "address": "4 Varet Street",
+    "city": "Breinigsville",
+    "state": "Oregon",
+    "zip": 54164,
+    "country": "Antarctica",
+    "email": "lavernehampton@scentric.com",
+    "phone": "(988) 478-3842",
+    "color": "rgb(225,78,134)"
+  },
+  {
+    "index": 14,
+    "name": "Key Keith",
+    "first": "Kirsten",
+    "last": "Chang",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/rafelorden/73.jpg",
+    "boolean": true,
+    "guid": "1e75c394-1574-4168-a33d-c5895f7af76d",
+    "integer": 47,
+    "date": "Tue Sep 18 1990 08:36:33 GMT-0700 (PDT)",
+    "address": "4 Ryder Avenue",
+    "city": "Wheatfields",
+    "state": "Illinois",
+    "zip": 28092,
+    "country": "Kiribati",
+    "email": "kirstenchang@scentric.com",
+    "phone": "(882) 531-2818",
+    "color": "rgb(118,83,134)"
+  },
+  {
+    "index": 15,
+    "name": "Lizzie Oneal",
+    "first": "Mercado",
+    "last": "Hancock",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/wim1k/73.jpg",
+    "boolean": false,
+    "guid": "8fbb9f3a-64df-4069-9aec-0d3dd0c2dd3f",
+    "integer": 22,
+    "date": "Thu Jan 02 1986 03:48:22 GMT-0800 (PST)",
+    "address": "4 Sumner Place",
+    "city": "Ruckersville",
+    "state": "West Virginia",
+    "zip": 65886,
+    "country": "Ecuador",
+    "email": "mercadohancock@scentric.com",
+    "phone": "(996) 557-3624",
+    "color": "rgb(244,178,231)"
+  },
+  {
+    "index": 16,
+    "name": "Bentley Guthrie",
+    "first": "Obrien",
+    "last": "Bruce",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/rpeezy/73.jpg",
+    "boolean": false,
+    "guid": "720e6095-dc65-4bef-b8bf-56b3e43c13e8",
+    "integer": 33,
+    "date": "Sat Sep 09 1989 04:51:19 GMT-0700 (PDT)",
+    "address": "2 Chester Street",
+    "city": "Finzel",
+    "state": "Mississippi",
+    "zip": 52444,
+    "country": "Bulgaria",
+    "email": "obrienbruce@scentric.com",
+    "phone": "(923) 461-2312",
+    "color": "rgb(52,103,133)"
+  },
+  {
+    "index": 17,
+    "name": "Marsh Cleveland",
+    "first": "Pearl",
+    "last": "Moran",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/sectronov/73.jpg",
+    "boolean": true,
+    "guid": "9b385d87-2847-44f4-acae-a47970ff7c9f",
+    "integer": 8,
+    "date": "Tue Apr 26 1988 18:46:50 GMT-0700 (PDT)",
+    "address": "4 Portland Avenue",
+    "city": "Henrietta",
+    "state": "Rhode Island",
+    "zip": 53764,
+    "country": "American Samoa",
+    "email": "pearlmoran@scentric.com",
+    "phone": "(964) 543-3486",
+    "color": "rgb(168,119,234)"
+  },
+  {
+    "index": 18,
+    "name": "Maude Perry",
+    "first": "Underwood",
+    "last": "Barton",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/starburst1977/73.jpg",
+    "boolean": true,
+    "guid": "0f6385b5-481e-4ddf-9ad4-b91f8e951922",
+    "integer": 33,
+    "date": "Wed Apr 23 2014 06:02:36 GMT-0700 (PDT)",
+    "address": "3 Mermaid Avenue",
+    "city": "Deputy",
+    "state": "Georgia",
+    "zip": 58449,
+    "country": "Angola",
+    "email": "underwoodbarton@scentric.com",
+    "phone": "(946) 414-3492",
+    "color": "rgb(112,197,153)"
+  },
+  {
+    "index": 19,
+    "name": "Knapp Pratt",
+    "first": "Solomon",
+    "last": "Lloyd",
+    "image": "https://s3.amazonaws.com/uifaces/faces/twitter/mocabyte/73.jpg",
+    "boolean": true,
+    "guid": "895a8dab-d037-46f4-b822-795814f418d9",
+    "integer": 72,
+    "date": "Thu Apr 13 1978 08:14:17 GMT-0800 (PST)",
+    "address": "4 Glenwood Road",
+    "city": "Berlin",
+    "state": "New Mexico",
+    "zip": 64625,
+    "country": "Bhutan",
+    "email": "solomonlloyd@scentric.com",
+    "phone": "(802) 539-3360",
+    "color": "rgb(69,68,176)"
+  }
+];
+
 var minidata =
 [
   {
@@ -674,31 +1058,31 @@ var minidata =
 ];
 
 document.addEventListener("WebComponentsReady", function() {
-  table1Fixture = document.getElementById('table1');
-  table1Fixture.tableData = data;
-  table2Fixture = document.getElementById('table2');
-  table2Fixture.tableData = minidata;
+  // table1Fixture = document.getElementById('table1');
+  // table1Fixture.tableData = data;
+  // table2Fixture = document.getElementById('table2');
+  // table2Fixture.tableData = minidata;
 
-  table3Fixture = document.getElementById('table3');
-  table3Fixture.tableData = data;
+  // table3Fixture = document.getElementById('table3');
+  // table3Fixture.tableData = data;
 
-  table4Fixture = document.getElementById('table4');
-  table4Fixture.tableData = data;
+  // table4Fixture = document.getElementById('table4');
+  // table4Fixture.tableData = data;
 
-  table5Fixture = document.getElementById('myTable');
-  table5Fixture.tableData = data;
+  // table5Fixture = document.getElementById('myTable');
+  // table5Fixture.tableData = data;
 
-  dropdownFixture = document.getElementById('myDropdownTable');
-  dropdownFixture.tableData = data;
+  // dropdownFixture = document.getElementById('myDropdownTable');
+  // dropdownFixture.tableData = data;
 
-  table6Fixture = document.getElementById('table6');
-  table6Fixture.tableData = data;
+  // table6Fixture = document.getElementById('table6');
+  // table6Fixture.tableData = data;
 
-  filtertest = document.getElementById('filtertest');
-  filtertest.tableData = minidata;
+  // filtertest = document.getElementById('filtertest');
+  // filtertest.tableData = minidata;
 
   serverTest = document.getElementById('server');
-  serverTest.tableData = data;
+  serverTest.tableData = dataTest;
 
 
   runTests();
@@ -1235,43 +1619,43 @@ function runTests() {
         var fixture = document.querySelector('#server');
         var firstNameHeaderSelector = '.aha-first-th > div > span';
         var firstNameHeader = fixture.querySelector(firstNameHeaderSelector);
-
         // Trigger a click on the First Name column header
         firstNameHeader.click();
-
         flush(function(){
           var tb = Polymer.dom(fixture.root).querySelector('aha-table'),
-            lastNameRow = Polymer.dom(tb.root).querySelectorAll('.aha-last-td');
+          lastNameRow = Polymer.dom(tb.root).querySelectorAll('.aha-last-td');
           //check if the first and the last item are at the same position
           assert.include(lastNameRow[0].textContent, 'Conrad');
           assert.include(lastNameRow[9].textContent, 'Duncan');
-          assert.include(lastNameRow[25].textContent, 'Lindsay');
+          assert.include(lastNameRow[6].textContent, 'Bartlett');
           done(); // end the test
         });
       });
 
       var eventData;
       document.querySelector('#server').addEventListener('px-data-table-state-changed', function(e){
+        console.log("2");
         eventData = e.detail;
       });
+      
       var serverPagination = document.querySelector('#server #pagination');
       // Selector for page 2 link
-      var page2Selector = '.paging.style-scope.px-pagination > span > :nth-child(1)';
+      var page2Selector = '.paging.style-scope.px-pagination > span > :nth-child(2)';
       test ('should not paginate',function(done){
         var fixture = document.querySelector('#server');
         var page2Button = serverPagination.querySelector(page2Selector);
         var table = Polymer.dom(fixture.root).querySelector('aha-table'),
         allLastNameRow = Polymer.dom(table.root).querySelectorAll('.aha-last-td');
-        //before page 2 is clicked, row should be 26
-        assert.equal(allLastNameRow.length, '26');
+        //before page 2 is clicked, row should be 20
+        assert.equal(allLastNameRow.length, '20');
         page2Button.click();
-        flush(function(){
-          assert.equal(allLastNameRow.length, '26');
-//          assert.equal(eventData.firstItemIndex, '21');
-//          assert.equal(eventData.sortedColumn, 'first');
-//          assert.equal(eventData.descending, false);
+        setTimeout(function(){
+         assert.equal(allLastNameRow.length, '20');
+         assert.equal(eventData.firstItemIndex, '21');
+         assert.equal(eventData.sortedColumn, 'first');
+         assert.equal(eventData.descending, true);
           done();
-        });
+        },15);
       });
 
       test('should emit table state changed event when sort on column',function(done){
@@ -1280,6 +1664,7 @@ function runTests() {
         var emailHeader = fixture.querySelector(emailHeaderSelector);
         emailHeader.click();
         setTimeout(function(){
+            debugger;
           assert.equal(eventData.firstItemIndex, '1');
           assert.equal(eventData.sortedColumn, 'email');
           assert.equal(eventData.descending, false);
@@ -1289,11 +1674,12 @@ function runTests() {
 
       test('should emit table state changed event when clicking on page number',function(done){
         var fixture = document.querySelector('#server');
-        var page1Selector = '.paging.style-scope.px-pagination > span > :nth-child(2)';
+        var page1Selector = '.paging.style-scope.px-pagination > span > :nth-child(1)';
         var page1Button = serverPagination.querySelector(page1Selector);
         page1Button.click();
         setTimeout(function(){
-          assert.equal(eventData.firstItemIndex, '21');
+            console.log("3");
+          assert.equal(eventData.firstItemIndex, '1');
           assert.equal(eventData.sortedColumn, 'email');
           assert.equal(eventData.descending, false);
           done();
